85b54c8f5e72e1208038934848f5171a010b4a9c
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-06-10 10:54:37 +0200

    Forgot to add updated csproj/sln files

--- Src/VTwin/Properties/AssemblyInfo.cs
+++ Src/VTwin/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VTwin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("VTwin - JIT Compiler for the DLR")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("IronJS")]
 [assembly: AssemblyProduct("VTwin")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Fredrik Holmström 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
