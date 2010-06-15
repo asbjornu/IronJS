79fe43fc410ab8a5bda47f593116538b54519092
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-06-15 19:24:10 +0200

    Split up Scope.cs into Scope/ScopeChain/VariableList.cs

---
+++ Src/IronJS/Compiler/Ast/Context/ScopeChain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronJS.Compiler.Ast.Context {
+    public class ScopeChain {
+        Stack<Scope> _scopes;
+
+        public Scope Current {
+            get { return _scopes.Peek(); }
+        }
+
+        public ScopeChain() {
+            _scopes = new Stack<Scope>();
+        }
+
+        public void Enter(Scope scope) {
+            _scopes.Push(scope);
+        }
+
+        public Scope Exit() {
+            return _scopes.Pop();
+        }
+    }
+}
