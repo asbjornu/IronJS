f8052a91260d3d7cadbccc598ab848d8c2c85c2d
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-06-15 12:24:18 +0200

    Renamed DefaultAnalyzer to just Default (it's already in the Analyzer namespace), Started implementing basic analyze functionality

--- Src/IronJS.REPL/Program.cs
+++ Src/IronJS.REPL/Program.cs
@@ -5,7 +5,7 @@ namespace IronJS.REPL {
 	class Program {
 		static void Main(string[] args) {
             var parser = new Compiler.Parser.AntlrEcma3();
-            var analyzer = new Compiler.Analyzer.DefaultAnalyzer();
+            var analyzer = new Compiler.Analyzer.Default();
 
             var nodes = parser.ParseFile("testing.js");
             var analyzed = analyzer.Analyze(nodes);
---
+++ Src/IronJS/Compiler/Analyzer/Default.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using IronJS.Compiler.Ast;
+using IronJS.Compiler.Ast.Nodes;
+
+namespace IronJS.Compiler.Analyzer {
+    public class Default : IAnalyzer {
+        public INode[] Analyze(INode[] nodes) {
+            var analyzedNodes = new INode[nodes.Length];
+
+            for(var i = 0; i < nodes.Length; ++i) {
+                analyzedNodes[i] = Analyze(nodes[i]);
+            }
+
+            return analyzedNodes;
+        }
+
+        INode Analyze(INode node) {
+            if (node is Var) {
+                return Analyze(node as Var);
+            } else {
+                return node;
+            }
+        }
+
+        INode Analyze(Var node) {
+            throw new NotImplementedException();
+        }
+    }
+}
