5a8d09492512a03813f12db17ac088a8a488d5d7
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-06-18 19:41:25 +0200

    Re-added VariableList.cs

---
+++ Src/IronJS/Compiler/Ast/Context/VariableList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronJS.Compiler.Ast.Nodes;
+
+namespace IronJS.Compiler.Ast.Context {
+    public class VariableList {
+        HashSet<Variable> _variables;
+
+        public VariableList() {
+            _variables = new HashSet<Variable>(new VariableComparer());
+        }
+
+        public void Add(Variable var) {
+            _variables.Add(var);
+        }
+
+        public Variable Get(string name) {
+            return _variables.First(x => x.Name == name);
+        }
+
+        public Variable Get(INode node) {
+            return _variables.First(x => x.Name == (node as Identifier).Name);
+        }
+
+        public void AddType(INode node, Runtime.Type type) {
+            Get(node).Type |= type;
+        }
+
+        public void AddAssignedFrom(INode node, INode value) {
+            if (value.TypeIsStatic) {
+                AddType(node, value.Type);
+            } else {
+                Get(node).AssignedFrom.Add(value);
+            }
+        }
+
+        class VariableComparer : IEqualityComparer<Variable> {
+            public bool Equals(Variable x, Variable y) { return x.Name == y.Name; }
+            public int GetHashCode(Variable obj) { return obj.Name.GetHashCode(); }
+        }
+    }
+}
