94ca42b197eea6f6b60fbc048759e8ad7dae312a
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-09-02 09:09:28 +0200

    Added Runtime.Box

---
+++ Src/IronJS/Runtime/Box.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace IronJS.Runtime {
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Box {
+        [FieldOffset(0)] object Clr;
+        [FieldOffset(8)] bool Bool;
+        [FieldOffset(8)] double Double;
+        [FieldOffset(16)] Type Type;
+    }
+}
